337ef20015da96f3e4bd8422a390c4b0f96f2f0c
Anh Phan <vietanhphan2810@gmail.com>
2025-03-27 09:01:55 +0000

    add PostSlider.js

---
+++ src/modules/PostSliders.js
@@ -0,0 +1,19 @@
+import Glide from "@glidejs/glide";
+
+class PostSlider{
+    constructor() {
+        if (document.querySelector(".bao-gia-dv")) {
+
+          const glide = new Glide(".bao-gia-dv", {
+            type: 'carousel',
+            startAt:0,
+            perView: 3,
+            autoplay: 3000
+          });
+
+          glide.mount();
+        }
+      }
+}
+
+export default PostSlider;
